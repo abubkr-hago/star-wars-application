552d1e26ce9713789aa3d003c81ea71a41b9d293
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2024-11-24 14:26:20 +0400

    ci: init tests and pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom', // Set to jsdom for React testing
+    globals: true, // This allows you to use `describe`, `it`, and `expect` globally
+    setupFiles: './setupTests.ts', // Path to your setup file
+    coverage: {
+      reporter: ['html'],
+      reportsDirectory: './coverage',
+    },
+  },
+});
